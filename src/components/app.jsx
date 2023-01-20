6b4695102dabba6fa4bfcbbcaca887fc74fc4aab
Joel Dario <joeldario9912@gmail.com>
2023-01-20 11:12:22 -0300

    cree usuario administrador para login

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -19,7 +19,9 @@ const App = () => {
     );  
   } else {
     return(
-      <BienvenidaComp />
+      <AppContext.Provider value={{ setUsuarioLogeado }}>
+        <BienvenidaComp />
+      </AppContext.Provider>
     )
   }
 }
--- src/components/comp-pages/bienvenida-comp/form-login.jsx
+++ src/components/comp-pages/bienvenida-comp/form-login.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React,{
+  useContext
+} from 'react';
 import InputPassword from '../../input-password';
 import InputSubmit from '../../input-submit';
 import InputText from '../../input-text';
-import '../../../styles/form-login.css'
+import '../../../styles/form-login.css';
+import { AppContext } from '../../app';
 
 const FormLogin = () => {
+  const { setUsuarioLogeado } = useContext(AppContext);
+  const logIn = (e) => {
+    const 
+      usuario = document.querySelector('[type="text"]'),
+      pass = document.querySelector('[type="password"]');
+    ;
+    e.preventDefault();
+
+    if (usuario.value === 'Pepo' && pass.value === 'admin') {
+      setUsuarioLogeado(true);
+    } else{
+      usuario.value = '';
+      pass.value = '';
+    }
+    
+  }
+
   return (
-    <form action="" className='form-login'>
+    <form action="" className='form-login' onSubmit={ logIn }>
       <InputText 
         texto='Usuario:' 
         placeholder='Ingresa tu usuario'
