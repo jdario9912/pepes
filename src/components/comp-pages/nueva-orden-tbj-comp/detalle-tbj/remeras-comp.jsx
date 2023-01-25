db0ff567af087fdfbdce69079b36b9e6e2208349
Joel Dario <joeldario9912@gmail.com>
2023-01-24 22:01:41 -0300

    configure componente remeras para recibir props

--- src/components/comp-pages/nueva-orden-tbj-comp/detalle-tbj/remeras-comp.jsx
+++ src/components/comp-pages/nueva-orden-tbj-comp/detalle-tbj/remeras-comp.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import InputText from '../../../input-text';
 import InputNumber from '../../../input-number';
+import { InputNumberModel } from '../../../../models/input-number-model';
+import { InputTextModel } from '../../../../models/input-text-model';
 
 const RemerasComp = () => {
   return (
     <div>
       <h5>Remeras</h5>
-      <InputText texto='Ubicación del archivo: ' />
-      <InputText texto='Talles: ' />
-      <InputText texto='Color: ' />
-      <InputText texto='Estampa pecho: ' />
-      <InputText texto='Estampa espalda: ' />
-      <InputText texto='Color estampa: ' />
-      <InputNumber texto='Cantidad: ' />
+      <InputText props={ new InputTextModel('Ubicación del archivo: ', '', '', 'Ingresa ubicación del archivo', '', 'remeras-ubicacion')} />
+      <InputText props={ new InputTextModel('Talles: ', '', '', 'Ingresa talles', '', 'remeras-talles')} />
+      <InputText props={ new InputTextModel('Color: ', '', '', 'Ingresa color', '', 'remeras-color')} />
+      <InputText props={ new InputTextModel('Estampa pecho: ', '', '', 'Ingresa estampa pecho', '', 'remeras-estampa-pecho')} />
+      <InputText props={ new InputTextModel('Estampa espalda: ', '', '', 'Ingresa estampa espalda', '', 'remeras-estampa-espalda')} />
+      <InputText props={ new InputTextModel('Color estampa: ', '', '', 'Ingresa color de la estampa', '', 'remeras-color-estampa')} />
+      <InputNumber props={ new InputNumberModel('Cantidad: ', '', '', '', 'remeras-cantidad')} />
     </div>
   );
 }
