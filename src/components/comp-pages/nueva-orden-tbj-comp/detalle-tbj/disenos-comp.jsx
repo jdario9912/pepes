964a33442877774d5c6dfbc1016ff4ca9b733f54
Joel Dario <joeldario9912@gmail.com>
2023-01-24 21:53:51 -0300

    configure componente plotter para recibir props

--- src/components/comp-pages/nueva-orden-tbj-comp/detalle-tbj/disenos-comp.jsx
+++ src/components/comp-pages/nueva-orden-tbj-comp/detalle-tbj/disenos-comp.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import InputText from '../../../input-text';
 import TextArea from '../../../text-area';
+import { InputTextModel } from '../../../../models/input-text-model';
+import { TextAreaModel } from '../../../../models/text-area-model';
 
 const DisenosComp = () => {
   return (
     <div>
       <h5>Diseños</h5>
-      <InputText texto='Ubicación archivo diseño: ' />
-      <TextArea texto='Detalles: ' />
-      <TextArea texto='Adjuntar bocetos en papel: ' />
+      <InputText props={ new InputTextModel('Ubicación del archivo: ', '', '', 'Ingresa ubicación del archivo', '', 'disenos-ubicacion-archivo')} />
+      <InputText props={ new InputTextModel('Detalles: ', '', '', 'Ingresa detalles del diseño', '', 'disenos-detalles')} />
+      <TextArea props={ new TextAreaModel('Descripción: ', '', '', 'Ingresa una descripción del diseño', '', 'diseno-descripcion')} />
     </div>
   );
 }
--- src/components/comp-pages/nueva-orden-tbj-comp/detalle-tbj/plotter-comp.jsx
+++ src/components/comp-pages/nueva-orden-tbj-comp/detalle-tbj/plotter-comp.jsx
@@ -3,13 +3,14 @@ import InputText from '../../../input-text';
 import Color from './plotter-comp/color';
 import Material from './plotter-comp/material';
 import Terminacion from './plotter-comp/terminacion';
+import { InputTextModel } from '../../../../models/input-text-model';
 
 const PlotterComp = () => {
   return (
     <div>
       <h5>Plotter</h5>
-      <InputText texto='Ubicación del archivo: ' />
-      <InputText texto='Tamaño: ' />
+      <InputText props={ new InputTextModel('Ubicación del archivo: ', '', '', 'Ingresa la ubicación del archivo', '', 'plotter-ubicacion')} />
+      <InputText props={ new InputTextModel('Tamaño: ', '', '', 'Ingresa el tamaño del plotter', '', 'plotter-tamano')} />
       <Color />
       <Material />
       <Terminacion />
