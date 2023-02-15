212e5607e2ecee43b5c874035b5bb84fad60c953
Joel Dario <joeldario9912@gmail.com>
2023-02-14 21:46:22 -0300

    capture el parametro para realizar la busqueda de orden o cliente

--- src/components/comp-pages/inicio-comp.jsx
+++ src/components/comp-pages/inicio-comp.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../../styles/inicio-comp.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Buscador from '../buscador';
+import ResultadosBusqueda from './inicio-comp/resultados-busqueda';
 
 const InicioComp = () => {
+  const useQuery = () => new URLSearchParams(useLocation().search);
+  const query = useQuery();
+  const busqueda = query.get('busqueda');
+
   return (
     <div className='inicio-comp--container'>
       <Buscador />
+      {
+        busqueda ? <ResultadosBusqueda /> : null
+      }
       <Outlet />
     </div>
   );
---
+++ src/components/comp-pages/inicio-comp/resultados-busqueda.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResultadosBusqueda = () => {
+  return (
+    <div>
+      ACA SE VAN A MOSTRAR LOS RESULTADOS DE LA BUSQUEDA
+    </div>
+  );
+}
+
+export default ResultadosBusqueda;
