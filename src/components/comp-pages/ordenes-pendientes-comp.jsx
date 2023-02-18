6b81e2bda61b39393c861a695f4bd12a4adafc9f
Joel Dario <joeldario9912@gmail.com>
2023-02-18 16:35:35 -0300

    ubique el lugar para consultar ordenes pendientes

--- src/components/comp-pages/ordenes-pendientes-comp.jsx
+++ src/components/comp-pages/ordenes-pendientes-comp.jsx
@@ -3,6 +3,7 @@ import MostrarOrdenesPendientes from '../comp-pages/ordenes-pendientes-comp/most
 import '../../styles/ordenes-pendientes-comp.css';
 
 const OrdenesPendientesComp = () => {
+  // ACA TENGO QUE CONSULTAR TODAS LAS ORDENES PENDIENTES
   return (
     <div className='ordenes-pendientes-comp--container modulo'>
       <h2>Ordenes Pendientes</h2>
