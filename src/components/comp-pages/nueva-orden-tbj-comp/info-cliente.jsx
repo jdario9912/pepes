4b9c756015ee0f4cde344196e6ef63338720fa6f
Joel Dario <joeldario9912@gmail.com>
2023-03-10 02:16:59 -0300

    cambie icono en info cliente

--- src/components/comp-pages/nueva-orden-tbj-comp/info-cliente.jsx
+++ src/components/comp-pages/nueva-orden-tbj-comp/info-cliente.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../../styles/info-cliente.css';
-import { BsTelephoneOutbound } from "react-icons/bs";
+import { MdPhoneEnabled } from "react-icons/md";
 import { MdAlternateEmail } from "react-icons/md";
 import { GiNotebook } from "react-icons/gi";
 
@@ -10,7 +10,7 @@ const InfoCliente = ({props}) => {
     <div className='info-cliente--datos-personales-container modulo'>
       <h3 className='titulo'>Nueva orden para <span className='nombre'>{ nombre }</span></h3>
       <div className="info-cliente--nombre-telefono-email">
-        { telefono !== '' ? <p><BsTelephoneOutbound className='icon' />{ ' ' + telefono }</p> : null }
+        { telefono !== '' ? <p><MdPhoneEnabled className='icon' />{ ' ' + telefono }</p> : null }
         { email !== '' ? <p><MdAlternateEmail className='icon' />{ ' ' + email }</p> : null }
         { observaciones !== '' ? <p className='info-cliente--observaciones'><GiNotebook className='icon' /> { observaciones }</p> : null}
       </div>
