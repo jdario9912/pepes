767166fa314e1d8bd3997966c9233cd1de08d72d
Joel Dario <joeldario9912@gmail.com>
2023-03-10 11:37:16 -0300

    no se que hice

--- src/components/comp-pages/nueva-orden-tbj-comp/detalle-tbj/talonarios-comp/tipo.jsx
+++ src/components/comp-pages/nueva-orden-tbj-comp/detalle-tbj/talonarios-comp/tipo.jsx
@@ -3,7 +3,7 @@ import SelectOption from '../../../../select-option';
 import { SelectOptionModel } from '../../../../../models/select-option-model';
 
 const Tipo = () => {
-  const opcionesTipo = ['Factura C', 'Recibo C', 'Remito R', 'Presupuesto X', 'Recibo X', 'Otros'];
+  const opcionesTipo = ['Factura C', 'Recibo C', 'Remito R', 'Presupuesto X', 'Recibo X', 'RP', 'Otros'];
   return (
     <div>
       <SelectOption props={ new SelectOptionModel('Tipo:', '', 'select', '', 'tipo', opcionesTipo) } />
