cb1a6596479b0a006961b7fc4358fadbe1c98954
Joel Dario <joeldario9912@gmail.com>
2023-03-27 13:31:14 -0300

    conectado a la api

--- src/services/url/url-api.js
+++ src/services/url/url-api.js
@@ -1,5 +1,5 @@
 const web = true;
 const local = 'http://localhost:3001';
-const railway = 'https://200.58.98.142';
+const railway = 'http://200.58.98.142';
 
 export const urlApi = web ? railway : local;
